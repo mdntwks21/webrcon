6d8f33ef314f490407ce73cf6037528c0cae1cf9
awoolen <airrickwoolen@yahoo.com>
2015-11-13 18:04:08 +0000

    Fixed server.js and add express to package.

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 "use strict";
 
 var express = require('express'),
-    api     = require('./api'),
     app     = express(),
     path    = require('path');
 
 app
     .use(express.static('./public'))
-    .use('/api', api)
     .get('*', function(req, res){
         res.sendFile(path.resolve('public/main.html'));
     })
