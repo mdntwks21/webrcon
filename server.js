0ac7f17b2a4fe4fee832be28109bf5d5fee17ce3
Raymond Say <say.raymond@gmail.com>
2015-12-07 16:30:50 -0500

    #16 Initial commit for Swagger API

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ var express = require('express'),
     app     = express(),
     path    = require('path'),
     os      = require('os'),
+    swagger = require('swagger-tools'),
+    swagDoc = require('./api/swagger.json'),
     options = require('./options.json');
     
 options.isWin = /^win/.test(os.platform());
+options.useStubs = process.env.NODE_ENV === 'development' ? true: false;
 
-app
-    .use(express.static('./public'))
-    .get('*', function(req, res){
-        res.sendFile(path.resolve('public/main.html'));
-    })
-    .listen(3000);
+swagger.initializeMiddleware(swagDoc, function( middleware ) {
+    app.use(middleware.swaggerMetadata());
+
+    app.use(middleware.swaggerValidator());
+
+    app.use(middleware.swaggerRouter(options));
+
+    app.use(middleware.swaggerUi());
+
+    app.listen(options.port);
+});
